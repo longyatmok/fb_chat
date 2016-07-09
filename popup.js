3a73fd2fc32812ccc48439fec10a7f5832ce8c82
Leonard Mok <longyatmok@gmail.com>
2016-07-09 12:00:59 +0800

    feat(list): filter, dom data-attribute, css

--- popup.js
+++ popup.js
@@ -1,19 +1,44 @@
-navCondition = true;
+listOfFriends = [
+  {"name": "Leonard Mok", "id": 1},
+  {"name": "Felix Yau", "id": 2},
+  {"name": "Adrian Ng", "id": 3},
+  {"name": "Henry Cheung", "id": 4},
+  {"name": "Richard Tsui", "id": 5},
+  {"name": "William Hi", "id": 6},
+  {"name": "Jim Ng", "id": 7},
+  {"name": "Sunny Ku", "id": 8},
+]
 
-/**
- * Toggle the view of nav
- * if True: login view, False: list view
- */
-function toggleNav() {
-    navCondition = !navCondition;
-    if (navCondition){
-        $('.content.list').hide();
-        $('.content.login').show();
-    } else {
-        $('.content.login').hide();
-        $('.content.list').show();
-    }
+function clickListName(e) {
+  console.log($(e.target).data("friendId"));
+  window.close();
 }
 
-// $('#list-nav').click(toggleNav);
-// $('#login-nav').click(toggleNav);
+function filterFriends(e) {
+  searchField = $(e.target).get(0);
+  searchValue = searchField.value.toLowerCase() ;
+  if(searchValue && searchValue.length > 0) {
+    newList = listOfFriends.filter(function(a){
+      return (a["name"].toLowerCase().indexOf(searchValue) > -1)
+    });
+    renderFriendList(newList);
+  } else {
+    renderFriendList(listOfFriends);
+  }
+}
+
+function renderFriendList(list){
+  listContainer = $('.list-container');
+  listContainer.empty();
+  list.forEach(function(friend, index){
+    var friendLi = $("<li></li>");
+    var aDom = $("<a></a>", {"data-friend-id": friend["id"]});
+    aDom.addClass('list-name');
+    aDom.text(friend["name"]);
+    friendLi.append(aDom);
+    listContainer.append(friendLi);
+  });
+}
+renderFriendList(listOfFriends);
+$('.search-field').on('input', filterFriends);
+$('.list-name').click(clickListName);
